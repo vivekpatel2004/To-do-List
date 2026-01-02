0cbd83fef7f71bd18a5ca84d607fdbd893c0eaa8
vivek4591 <155885616+vivekpatel2004@users.noreply.github.com>
2026-01-02 11:34:41 +0530

    Secure DB config using env variables

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,13 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # loads .env variables
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="VVcc@4591",
-        database="todo_pro"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
